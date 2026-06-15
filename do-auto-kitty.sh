3b141d03fb9fb100b84ef5a9f1ddf2b2a99df7de
GreedlyCore <naganariselves@gmail.com>
2026-06-16 01:57:08 +0300

    something new

---
+++ do-auto-kitty.sh
@@ -0,0 +1,6 @@
+# Auto-setup and install kitty+nvim+zsh+fzf
+# Swap Kitty with Ghostty, or make it optionable in the future.
+# Requirements: Git and Python 3 must be installed.
+git clone https://github.com/Juanfu224/Auto-Kitty-Workspace.git ~/Auto-Kitty-Workspace
+cd ~/Auto-Kitty-Workspace
+python3 main.py
